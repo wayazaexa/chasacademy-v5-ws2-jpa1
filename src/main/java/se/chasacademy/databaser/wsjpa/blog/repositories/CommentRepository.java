199226c090b1a9286d6e828063a2ccc889b098a3
Joakim Englund <joakimenglund@protonmail.com>
2025-12-11 14:12:37 +0100

    Added comment repository with first function

--- src/main/java/se/chasacademy/databaser/wsjpa/blog/repositories/CommentRepository.java
+++ src/main/java/se/chasacademy/databaser/wsjpa/blog/repositories/CommentRepository.java
@@ -1,4 +1,12 @@
 package se.chasacademy.databaser.wsjpa.blog.repositories;
 
-public class CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.chasacademy.databaser.wsjpa.blog.models.Comment;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    //- hämta kommentarer per inlägg
+    List<Comment> findAllByPostId(int postId);
 }
